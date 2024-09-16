1400f7eebb537001bbbb841cb3b62ff038092163
Kelly Hayd <hayd.kelly@gmail.com>
2024-09-15 21:19:11 -0300

    use toupper

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,23 +3,19 @@
 #include <string>
 
 int main(int argc, char **argv) {
+	std::string	result;
+
 	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << '\n';
 		return (0);
 	}
-	int	j = 1;
-	while (j < argc) {
-		std::string	str = argv[j];
-		long unsigned int	i = 0;
-		while (i < str.length() && !str.empty()) {
-			if (str.at(i) >= 97 && str.at(i) <= 122)
-				str.at(i) -= 32;
-			i++;
+	for (int i = 1; i < argc; i++) {
+		std::string str(argv[i]);
+		for (int j = 0; j < str.size(); j++) {
+			str[j] = toupper(str[j]);
 		}
-		std::cout << str << ' ';
-		str.clear();
-		j++;
+		result += str;
 	}
-	std::cout << std::endl;
+	std::cout << result << '\n';
 	return (0);
 }

f88f755705c5a64586c612fb4e5f61923b310cdd
Kelly Hayd <hayd.kelly@gmail.com>
2024-11-15 08:25:01 -0300

    FEATURE: update program: print correction of address of reference

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -2,12 +2,18 @@
 #include <string>
 
 int	main(){
-	std::string		str = "HI THIS IS BRAIN";
-	std::string*	ptr = &str;
+	std::string		string = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string		&stringREF = string;
 
-	std::cout << str << std::endl;
-	std::cout << &str << std::endl;
-	std::cout << ptr << std::endl;
-	
+	std::cout << "Memory address of string: " << &string << std::endl;
+	std::cout << "Memory address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address of stringREF: " << &stringREF << std::endl;
+	std::cout << "\n";
+
+	std::cout << "Value of string: " << string << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+
 	return (0);
 }

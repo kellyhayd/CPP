1b2b6481c58f9cf281cdf65b0a057e854ac51359
Kelly Hayd <hayd.kelly@gmail.com>
2024-11-19 00:50:07 -0300

    FEATURE: exercise 5

---
+++ CPP01/ex05/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Harl.hpp"
+
+int	main(){
+	Harl harl;
+
+	std::cout << std::endl << "------ DEBUG Level ------" << std::endl;
+	harl.complain("DEBUG");
+	std::cout << std::endl << "------ INFO Level ------" << std::endl;
+	harl.complain("INFO");
+	std::cout << std::endl << "------ WARNING Level ------" << std::endl;
+	harl.complain("WARNING");
+	std::cout << std::endl << "------ ERROR Level ------" << std::endl;
+	harl.complain("ERROR");
+	std::cout << std::endl << "------ No valid Level ------" << std::endl;
+	harl.complain("42");
+
+	return (0);
+}

9a56edecda9c375e8e3165987ed7bd5941873e17
Kelly Rocha Hayd <krocha-h@c1r6s1.42london.com>
2025-01-15 15:23:05 +0000

    FEATURE: include getters and setters

--- CPP03/ex00/include/ClapTrap.hpp
+++ CPP03/ex00/include/ClapTrap.hpp
@@ -28,9 +28,14 @@ public:
 	ClapTrap& operator=(const ClapTrap& other);
 	~ClapTrap();
 
-	// void	attack(const std::string& target);
-	// void	takeDamage(unsigned int amount);
-	// void	beRepaired(unsigned int amount);
+	std::string	getName();
+	int			getAttackDamage();
+	void		setHitPoints(int amount);
+	void		setEnergyPoints(int amount);
+
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
 };
 
 #endif

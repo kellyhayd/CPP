c165417cfcf64af47bb6fbeb13091ea429de102d
Kelly Rocha Hayd <krocha-h@c1r6s1.42london.com>
2025-01-16 10:27:34 +0000

    STYLE: update on tests

--- CPP02/ex03/src/main.cpp
+++ CPP02/ex03/src/main.cpp
@@ -4,21 +4,24 @@ bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 void test1(void) {
 	if (bsp(Point(0, 0), Point(10, 30), Point(20, 0), Point(10, 15)))
-		std::cout << "Point is in the triangle" << std::endl;
+		std::cout << "Point is inside the triangle" << std::endl;
 	else
-		std::cout << "Point is not in the triangle" << std::endl;
+		std::cout << "Point is outside the triangle" << std::endl;
 }
 
 void test2(void) {
 	if (bsp(Point(0, 0), Point(10, 30), Point(20, 0), Point(30, 15)))
-		std::cout << "Point is in the triangle" << std::endl;
+		std::cout << "Point is inside the triangle" << std::endl;
 	else
-		std::cout << "Point is not in the triangle" << std::endl;
+		std::cout << "Point is outside the triangle" << std::endl;
 }
 
 int main(void) {
+	// the point is inside the triangle
 	test1();
+
+	// the point is outside the triangle
 	test2();
 
-	return EXIT_SUCCESS;
+	return (0);
 }

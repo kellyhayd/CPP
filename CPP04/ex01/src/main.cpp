454dc13c544e089a59370931a5ec5bc96e0391f0
Kelly Rocha Hayd <krocha-h@c1r2s1.42london.com>
2025-01-22 18:28:57 +0000

    STYLE: main update

--- CPP04/ex01/src/main.cpp
+++ CPP04/ex01/src/main.cpp
@@ -4,7 +4,7 @@
 
 int main(void) {
 	{
-		std::cout << INVERSE << "\n SUBJECT TESTS \n" << RESET << std::endl;
+		std::cout << INVERSE << "\n  SUBJECT TESTS  \n" << RESET << std::endl;
 
 		const Animal *j = new Dog();
 		const Animal *i = new Cat();
@@ -13,7 +13,7 @@ int main(void) {
 	}
 
 	{
-		std::cout << INVERSE << "\n SOME TESTS \n" << RESET << std::endl;
+		std::cout << INVERSE << "\n  MORE TESTS  \n" << RESET << std::endl;
 
 		const int arraySize = 4;
 		Animal *animals[arraySize];
@@ -22,20 +22,31 @@ int main(void) {
 			animals[i * 2] = new Dog();
 			animals[i * 2 + 1] = new Cat();
 		}
+		std::cout << std::endl;
+
+		std::cout << BOLD << "\nDeleting animals\n" << RESET << std::endl;
 
 	/* Delete animals */
 		for (int i = 0; i < arraySize; ++i) {
 			delete animals[i];
 		}
+		std::cout << std::endl;
 	}
 
 	std::cout << std::endl;
 
 	{
-		Dog basic;
-		{ Dog tmp = basic; }
-		std::cout << basic.getType() << std::endl;
-		basic.makeSound();
+		std::cout << INVERSE << "\n  COPY TESTS  \n" << RESET << std::endl;
+
+		std::cout << BOLD << "\nCreating a copy\n" << RESET << std::endl;
+		Dog original;
+		{
+			Dog copy = original;
+			std::cout << copy.getType() << std::endl;
+			copy.makeSound();
+		}
+		std::cout << "\n" << original.getType() << std::endl;
+		original.makeSound();
 	}
 
 	return (0);

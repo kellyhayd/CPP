5c2bb1c02cebcf6d474e1027940518a24a030499
Kelly Rocha Hayd <krocha-h@c1r6s1.42london.com>
2025-01-30 16:53:32 +0000

    FEATURE (WIP): exercise 00

---
+++ CPP06/ex00/include/ScalarConverter.hpp
@@ -0,0 +1,14 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter {
+public:
+	~ScalarConverter();
+
+	static void	convert(std::string value);
+};
+
+
+#endif
---
+++ CPP06/ex00/src/ScalarConverter.cpp
@@ -0,0 +1,10 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::~ScalarConverter() {};
+
+void	ScalarConverter::convert(std::string value) {
+	
+}
+
+
+// printable ascii 33-126

77ffb6d4e9b35602b494c1e137c661809b1813f9
Kelly Rocha Hayd <krocha-h@c1r6s1.42london.com>
2025-02-19 14:31:16 +0000

    FEATURE: addRange: addRange in hpp file; maxSize to unsigned int; main tests that creates a range of random numbers

--- CPP08/ex01/include/Span.hpp
+++ CPP08/ex01/include/Span.hpp
@@ -7,7 +7,7 @@
 class Span {
 private:
 	std::vector<int>	_content;
-	size_t	_maxSize;
+	unsigned int	_maxSize;
 
 public:
 	Span(unsigned int N);
@@ -18,6 +18,15 @@ public:
 	void	addNumber(int value);
 	int	shortestSpan();
 	int	longestSpan();
+
+	template <typename T>
+	void addRange(T begin, T end) {
+		if (std::distance(begin, end) > _maxSize \
+			|| std::distance(begin, end) > (_maxSize - static_cast<unsigned int>(_content.size()))) {
+			throw std::overflow_error("There is not enough space for this range");
+		}
+		_content.insert(_content.end(), begin, end);
+	};
 
 	std::vector<int>	getContent();
 };
--- CPP08/ex01/src/main.cpp
+++ CPP08/ex01/src/main.cpp
@@ -1,30 +1,65 @@
 #include "Span.hpp"
 #include "colors.hpp"
+#include <cstdlib>
+
+std::vector<int>	createRange(int N) {
+	std::vector<int>	range;
+	srand(time(NULL));
+	for (int i = 0; i < N; i++) {
+		range.push_back(rand() % N);
+	}
+	return (range);
+}
 
 int	main() {
-	Span	span(5);
+	// filling the class container
+	Span	span1(5);
 	int	values[] = {6, 3, 17, 9, 11};
 	for (int i = 0; i < 5; i++) {
-		span.addNumber(values[i]);
+		span1.addNumber(values[i]);
 	}
-
-	std::vector<int>	content = span.getContent();
-	std::cout << BOLD << "span =";
+	std::vector<int>	content = span1.getContent();
+	std::cout << BOLD << "span1 =";
 	for (std::vector<int>::iterator it = content.begin(); it != content.end(); it++) {
 		std::cout << " " << YELLOW << *it;
 	}
 	std::cout << RESET << std::endl;
 
+	// trying to insert number without space
 	int	newValue = 15;
 	try {
-		span.addNumber(newValue);
+		span1.addNumber(newValue);
 	} catch(std::exception& e) {
 		std::cout << BOLD << RED << e.what() << RESET << std::endl;
 	}
 	std::cout << RESET << std::endl;
 
-	std::cout << BOLD << "shortest span: " << YELLOW << span.shortestSpan() << RESET << std::endl;
-	std::cout << BOLD << "longest span: " << YELLOW << span.longestSpan() << RESET << std::endl;
+	// finding the shortest and longest span
+	std::cout << BOLD << "shortest span: " << YELLOW << span1.shortestSpan() << RESET << std::endl;
+	std::cout << BOLD << "longest span: " << YELLOW << span1.longestSpan() << RESET << std::endl;
+	std::cout << RESET << std::endl;
+
+	// creating a range of number
+	std::vector<int>	range = createRange(15000);
+	std::cout << BOLD << "range:" << RESET;
+	for (std::vector<int>::iterator it = range.begin(); it != (range.begin() + 5); it++) {
+		std::cout << " " << YELLOW << *it;
+	}
+	std::cout << RESET << std::endl;
+
+	// adding the range of number to the container
+	Span	span2(15000);
+	span2.addRange(range.begin(), range.end());
+	std::vector<int> span2Content = span2.getContent();
+	std::cout << BOLD << "span2:" << RESET;
+	for (std::vector<int>::iterator it = span2Content.begin(); it != (span2Content.begin() + 5); it++) {
+		std::cout << " " << YELLOW << *it;
+	}
+	std::cout << RESET << std::endl;
+
+	// finding the shortest and longest span
+	std::cout << BOLD << "shortest span: " << YELLOW << span2.shortestSpan() << RESET << std::endl;
+	std::cout << BOLD << "longest span: " << YELLOW << span2.longestSpan() << RESET << std::endl;
 	std::cout << RESET << std::endl;
 
 	return (0);

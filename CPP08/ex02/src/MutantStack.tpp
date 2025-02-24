a53f9568b50426b1535af6cce0c19c3d4f894220
Kelly Rocha Hayd <krocha-h@c1r5s1.42london.com>
2025-02-24 16:47:47 +0000

    FEATURE: tests in main

--- CPP08/ex02/src/MutantStack.tpp
+++ CPP08/ex02/src/MutantStack.tpp
@@ -5,9 +5,7 @@ template<typename T> MutantStack<T>::MutantStack() : std::stack<T>() {};
 template<typename T> MutantStack<T>::MutantStack(const MutantStack& other) : std::stack<T>(other) {};
 
 template<typename T> MutantStack<T>& MutantStack<T>::operator=(const MutantStack<T>& other) {
-	if (this != &other) {
-		std::stack<T>::operator=(other);
-	}
+	std::stack<T>::operator=(other);
 	return (*this);
 };
 
--- CPP08/ex02/src/main.cpp
+++ CPP08/ex02/src/main.cpp
@@ -1,36 +1,72 @@
 #include "MutantStack.hpp"
+#include "colors.hpp"
 #include <list>
+#include <typeinfo>
+
+
+template<typename T>void	printStack(MutantStack<T> stack, std::string type) {
+	MutantStack<int>::iterator it = stack.begin();
+	MutantStack<int>::iterator ite = stack.end();
+	std::cout << BOLD << "stack [" << MAGENTA << type << RESET << BOLD << "] = { ";
+	while (it != ite) {
+		std::cout << YELLOW << *it << " " << RESET;
+		++it;
+	}
+	std::cout << BOLD <<  "}" << RESET << std::endl;
+}
 
 int main() {
-	MutantStack<int> mstack;
+	std::cout << INVERSE << "   SUBJECT TEST   " << RESET << std::endl;
+	MutantStack<int>	mstack;
 	mstack.push(5);
-	mstack.push(17);
-	// {17, 5}
-	std::cout << mstack.top() << std::endl; // 17
+	mstack.push(17); // {17, 5}
+	std::cout << BOLD << "top: " << YELLOW << mstack.top() << RESET << std::endl; // 17
 
-	mstack.pop();
-	// {5}
-	std::cout << mstack.size() << std::endl; // == 1
+	mstack.pop(); // {5}
+	std::cout << BOLD << "size: " << YELLOW << mstack.size() << RESET << std::endl; // == 1
 
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	mstack.push(0);
-	// {5, 3, 5, 737, 0}
+	mstack.push(0); // {5, 3, 5, 737, 0}
 	MutantStack<int>::iterator it = mstack.begin();
-	std::cout << *it << std::endl; // test
 	MutantStack<int>::iterator ite = mstack.end();
-	std::cout << *ite << std::endl; // test
-
 	++it;
 	--it;
-
+	std::cout << BOLD << "stack: { ";
 	while (it != ite) {
-		std::cout << *it << std::endl;
+		std::cout << YELLOW << *it << " " << RESET;
 		++it;
 	}
-	std::list<int> list;
-	
+	std::cout << BOLD <<  "}" << RESET << std::endl;
+	std::stack<int> s(mstack);
+	std::cout << std::endl;
+
+	std::cout << INVERSE << "   LIST TEST   " << RESET << std::endl;
+	std::list<int> 	list;
+	list.push_back(5);
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	list.push_back(0);
+
+	std::cout << BOLD << "list: { ";
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); it++) {
+		std::cout << YELLOW << *it << " " << RESET;
+	}
+	std::cout << "}" << RESET << std::endl;
+	std::cout << std::endl;
+
+
+	std::cout << INVERSE << "   COPY CONSTRUCTOR && ASSIGNMENT OPERATOR   " << RESET << std::endl;
+	MutantStack<int>	stackCopy(mstack);
+	MutantStack<int>	stackOperator = stackCopy;
+	printStack(mstack, "original");
+	printStack(stackCopy, "copy");
+	printStack(stackOperator, "assignment operator");
+	std::cout << std::endl;
+
+
 	return (0);
 }
 

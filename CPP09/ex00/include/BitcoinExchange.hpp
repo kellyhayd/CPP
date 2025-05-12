7add29f724ea0e35eda90f3c0c54adf16f1d4d14
Kelly Rocha Hayd <krocha-h@c1r6s1.42london.com>
2025-05-12 15:03:14 +0100

    feature(wip): map has a struct as date; new functions = validateDate, valiateValue, printOutput and findMatch; final value is being rounded

--- CPP09/ex00/include/BitcoinExchange.hpp
+++ CPP09/ex00/include/BitcoinExchange.hpp
@@ -4,11 +4,27 @@
 #include <iostream>
 #include <map>
 
+typedef struct s_date {
+	int	year;
+	int	month;
+	int	day;
+
+	bool operator<(const s_date& other) const {
+		if (year != other.year)
+		    return year < other.year;
+		if (month != other.month)
+		    return month < other.month;
+		return day < other.day;
+    }
+} t_date;
 
 class BitcoinExchange {
 private:
-	std::map<std::string, float>	_data;
-
+	std::map<t_date, float>	_data;
+	int		_year;
+	int		_month;
+	int		_day;
+	float	_value;
    
 public:
 	BitcoinExchange();
@@ -16,8 +32,12 @@ public:
 	BitcoinExchange& operator=(const BitcoinExchange &copy);
 	~BitcoinExchange();
 
-	std::map<std::string, float>	getData() const;
+	std::map<t_date, float>	getData() const;
 	void	processInput(std::ifstream& file);
+	bool	validateDate(std::string& date);
+	bool	validateValue(std::string& value, std::string& line);
+	void	printOutput(std::string& date, std::string& value);
+	float	findMatch();
 };
 
 
--- CPP09/ex00/src/BitcoinExchange.cpp
+++ CPP09/ex00/src/BitcoinExchange.cpp
@@ -2,8 +2,10 @@
 #include "colors.hpp"
 
 #include <fstream>
+#include <stdlib.h>
+#include <string>
 
-static void	fillData(std::map<std::string, float>& data) {
+static void	fillData(std::map<t_date, float>& data) {
 	std::ifstream	file("data.csv");
 	if (file.is_open()) {
 		std::string	line;
@@ -20,7 +22,10 @@ static void	fillData(std::map<std::string, float>& data) {
 				std::cerr << "Error: Negative rate" << std::endl;
 				continue;
 			}
-			data[date] = rate;
+			t_date key = {atoi(date.substr(0, date.find('-')).c_str()), 
+				atoi(date.substr(date.find('-') + 1, date.rfind('-') - date.find('-') - 1).c_str()), 
+				atoi(date.substr(date.rfind('-') + 1).c_str())};
+			data[key] = rate;
 		}
 	}
 }
@@ -46,15 +51,87 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange &copy) {
 
 BitcoinExchange::~BitcoinExchange() {}
 
-std::map<std::string, float>	BitcoinExchange::getData() const { return (this->_data); }
+std::map<t_date, float>	BitcoinExchange::getData() const { return (this->_data); }
 
 void	BitcoinExchange::processInput(std::ifstream& file) {
 	std::string	line;
 	std::getline(file, line);
+	std::getline(file, line);
 	std::string	date = line.substr(0, line.find('|'));
+	if (!validateDate(date)) {
+		std::cerr << RED("Error: bad input => ") << line << std::endl;
+	}
+	std::string value = line.substr(line.find('|') + 1);
+	if (validateValue(value, line))
+		printOutput(date, value);
+}
+
+bool	BitcoinExchange::validateDate(std::string& date) {
 	if (date.empty()) {
-		// invalid input
-	} else if (!validateDate(date)) {
-		// invalid data
-	} 
+		return (false);
+	}
+	for (std::string::iterator it = date.begin();  it != date.end(); it++) {
+		if (std::isdigit(*it)) {
+			break;
+		}
+	}
+	
+	size_t firstDash = date.find('-');
+	size_t secondDash = date.find('-', firstDash + 1);
+	if (firstDash == std::string::npos || secondDash == std::string::npos) {
+		return (false);
+	}
+	_year = atoi(date.substr(0, firstDash).c_str());
+	if (_year > 2025 || _year < 2009) {
+		return (false);
+	}
+	_month = atoi(date.substr(firstDash + 1, secondDash - firstDash - 1).c_str());
+	if (_month > 12 || _month < 1) {
+		return (false);
+	}
+	_day = atoi(date.substr(secondDash + 1).c_str());
+	if (_day < 1 || _day > 31) {
+		return (false);
+	}
+	if (((_month == 2 && _day > 29) || (_month == 2 && _day == 29 && _year % 4 != 0)) ||
+		((_month == 4 || _month == 6 || _month == 9 || _month == 11) && _day > 30)) {
+		return (false);
+	}
+	return (true); 
+}
+
+bool	BitcoinExchange::validateValue(std::string& value, std::string& line) {
+	_value = std::strtof(value.c_str(), 0);
+	if (!_value) {
+		std::cerr << RED("Error: bad input => ") << line << std::endl;
+		return (false);
+	} else if (_value < 0) {
+		std::cerr << RED("Error: not a positive number") << std::endl;
+		return (false);
+	} else if (_value > 1000) {
+		std::cerr << RED("Error: too large a number") << std::endl;
+		return (false);
+	}
+	return (true);
+}
+
+void	BitcoinExchange::printOutput(std::string& date, std::string& value) {
+	float rate = findMatch();
+	float result = rate * _value;
+
+	std::cout << YELLOW(date) << " => " << YELLOW(value) << " = " << CYAN(result);
+}
+
+float	BitcoinExchange::findMatch() {
+	int	closestRate = 0;
+	for (std::map<t_date, float>::iterator it = _data.begin(); it != _data.end(); it++) {
+		if (it->first.year == _year && it->first.month == _month && it->first.day == _day) {
+			return (it->second);
+		}
+		if (it->first.year < _year || (it->first.year == _year && it->first.month < _month) ||
+			(it->first.year == _year && it->first.month == _month && it->first.day < _day)) {
+			closestRate = it->second;
+		}
+	}
+	return (closestRate);
 }

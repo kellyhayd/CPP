fb339069061f08946a298392660a09bf6b61e0fd
Kelly Rocha Hayd <krocha-h@c1r6s1.42london.com>
2025-05-12 15:15:50 +0100

    style: remove bold from colors

--- CPP09/ex00/include/colors.hpp
+++ CPP09/ex00/include/colors.hpp
@@ -1,13 +1,13 @@
 #ifndef COLORS_HPP
 #define COLORS_HPP
 
-#define BLACK(text) "\033[1;30m" << text << "\033[0m"
-#define RED(text) "\033[1;31m" << text << "\033[0m"
-#define GREEN(text) "\033[1;32m" << text << "\033[0m"
-#define YELLOW(text) "\033[1;33m" << text << "\033[0m"
-#define BLUE(text) "\033[1;34m" << text << "\033[0m"
-#define MAGENTA(text) "\033[1;35m" << text << "\033[0m"
-#define CYAN(text) "\033[1;36m" << text << "\033[0m"
-#define WHITE(text) "\033[1;37m" << text << "\033[0m"
+#define BLACK(text) "\033[30m" << text << "\033[0m"
+#define RED(text) "\033[31m" << text << "\033[0m"
+#define GREEN(text) "\033[32m" << text << "\033[0m"
+#define YELLOW(text) "\033[33m" << text << "\033[0m"
+#define BLUE(text) "\033[34m" << text << "\033[0m"
+#define MAGENTA(text) "\033[35m" << text << "\033[0m"
+#define CYAN(text) "\033[36m" << text << "\033[0m"
+#define WHITE(text) "\033[37m" << text << "\033[0m"
 
 #endif
